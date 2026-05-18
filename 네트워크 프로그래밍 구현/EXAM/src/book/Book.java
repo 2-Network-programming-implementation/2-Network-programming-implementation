171301803ea76aa71b9b038e00810af04a25c085
pyj-0313 <pyjun0313@gmail.com>
2026-05-18 15:13:40 +0900

    private 필드 6개 선언(수정2)

--- 네트워크 프로그래밍 구현/EXAM/src/book/Book.java
+++ 네트워크 프로그래밍 구현/EXAM/src/book/Book.java
@@ -38,55 +38,14 @@ package book;
 public class Book {
 
     // TODO: private 필드 6개 선언
-	private String book_code;
-	private int Classification_Id;
+	private String bookCode;
+	private Integer classificationId;
 	private String author;
 	private String name;
 	private String publisher;
-	private String isreserve;
+	private String isReserve;
 
-    /** 기본 생성자 — 그대로 사용 */
-    public Book() {}
 
-    /**
-     * 전체 인자 생성자 — 학생 TODO: 매개변수 6개로 모든 필드 초기화
-     */
-    public Book(String bookCode, Integer classificationId,
-                String author, String name, String publisher, String isReserve) {
-        // TODO: 6개 필드 초기화 (this.xxx = xxx)
-    	this.book_code = book_code;
-    	this.Classification_Id = Classification_Id;
-    	this.author = author;
-    	this.name = name;
-    	this.publisher = publisher;
-    	this.isreserve = isreserve;
-    }
 
-    // ====== getter / setter — 학생 TODO: 본문 채우기 (시그니처는 변경 금지) ======
 
-    public String  getBookCode()                  { return book_code; }
-    public void    setBookCode(String v)          { this.book_code = v; }
-
-    public Integer getClassificationId()          { return Classification_Id; }
-    public void    setClassificationId(Integer v) { this.Classification_Id = v; }
-
-    public String  getAuthor()                    { return author; }
-    public void    setAuthor(String v)            { this.author = v; }
-
-    public String  getName()                      { return name; }
-    public void    setName(String v)              { this.name = v; }
-
-    public String  getPublisher()                 { return publisher; }
-    public void    setPublisher(String v)         { this.publisher = v; }
-
-    public String  getIsReserve()                 { return isreserve; }
-    public void    setIsReserve(String v)         { this.isreserve = v; }
-    
-    
-
-	/** 학생 TODO: 모든 필드 표시 (디버깅 용) */
-    @Override
-    public String toString() {
-        return "Book{TODO: 모든 필드 표시}";
-    }
 }
