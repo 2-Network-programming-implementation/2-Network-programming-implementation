d046f9586d9ec6cb3b443e06368e9832b9ea6fc3
byeongseon <byeongseon8@gmail.com>
2026-05-18 16:25:06 +0900

    ServerSocket 자원 해제 (null 체크 + try-catch)

--- 네트워크 프로그래밍 구현/EXAM/src/chat/ChatServer.java
+++ 네트워크 프로그래밍 구현/EXAM/src/chat/ChatServer.java
@@ -85,8 +85,7 @@ public class ChatServer {
      */
     public static void broadcast(String message) {
         // TODO: 동기화 블록 안에서 clients 순회 → 각 handler.send(message) 호출
-        System.out.println("[broadcast TODO] " + message);
-    }
+    	
 
     /** 클라이언트 접속 종료 시 컬렉션에서 제거 */
     public static void remove(ChatHandler handler) {
